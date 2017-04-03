967f12c19274ec98fb0ad53b87cb52d2075a64d1
Vincent <vincent@Mac-mini-de-Zroud.local>
2017-04-03 19:06:04 +0200

    MainActivity

---
+++ app/src/main/java/igraal/com/poc_deezer_vincent/Activity/MainActivity.java
@@ -0,0 +1,17 @@
+package igraal.com.poc_deezer_vincent.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by vincent on 03/04/2017.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
