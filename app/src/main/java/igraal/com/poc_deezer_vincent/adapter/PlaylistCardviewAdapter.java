de7c308e4492878c0352e1ab98a707f3d8689dd0
Vincent Tarreau <vtarreau@igraal.com>
2017-04-18 15:47:28 +0200

    +[IGAND-672] add view holder class

--- app/src/main/java/igraal/com/poc_deezer_vincent/adapter/PlaylistCardviewAdapter.java
+++ app/src/main/java/igraal/com/poc_deezer_vincent/adapter/PlaylistCardviewAdapter.java
@@ -5,14 +5,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import igraal.com.poc_deezer_vincent.R;
+import igraal.com.poc_deezer_vincent.adapter.viewholder.PlaylistCardViewAdapterViewHolder;
 import igraal.com.poc_deezer_vincent.object.realmobject.RealmPlaylist;
 import io.realm.RealmList;
 
@@ -20,25 +17,10 @@ import io.realm.RealmList;
  * Created by vincent on 13/04/2017.
  */
 
-public class PlaylistCardViewAdapter extends RecyclerView.Adapter<PlaylistCardViewAdapter.ViewHolder> {
+public class PlaylistCardViewAdapter extends RecyclerView.Adapter<PlaylistCardViewAdapterViewHolder> {
 
     private RealmList <RealmPlaylist> playlist;
     private Context context;
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-
-        @BindView(R.id.display_user_playlist_cardview_title)
-        public TextView playlistTitle;
-        @BindView(R.id.display_user_playlist_cardview_tracks)
-        public TextView playlistTracksNb;
-        @BindView(R.id.display_user_playlist_cardview_image)
-        public ImageView playlistImageView;
-
-        public ViewHolder(View view) {
-            super(view);
-            ButterKnife.bind(this, view);
-        }
-    }
 
     public PlaylistCardViewAdapter(RealmList<RealmPlaylist> playlist, Context context) {
         this.playlist = playlist;
@@ -46,15 +28,15 @@ public class PlaylistCardViewAdapter extends RecyclerView.Adapter<PlaylistCardVi
     }
 
     @Override
-    public PlaylistCardViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public PlaylistCardViewAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.playlist_cardview, parent, false);
-        ViewHolder vh = new ViewHolder(v);
+        PlaylistCardViewAdapterViewHolder vh = new PlaylistCardViewAdapterViewHolder(v);
         return vh;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(PlaylistCardViewAdapterViewHolder holder, int position) {
         holder.playlistTitle.setText(playlist.get(position).getTitle());
         holder.playlistTracksNb.setText(Integer.toString(playlist.get(position).getNb_tracks()));
         Glide
