4fb1ae31303c2d059cd0f89106761a008db684f7
Athanase <akalantzakis@igraal.com>
2019-02-13 17:29:10 +0100

    * Fix merge

--- app/src/main/java/fr/athanase/deezer/dao/DeezerDao.kt
+++ app/src/main/java/fr/athanase/deezer/dao/DeezerDao.kt
@@ -1,223 +1,124 @@
 package fr.athanase.deezer.dao
 
 import io.realm.Realm
+import io.realm.RealmConfiguration
 import io.realm.RealmObject
 import io.realm.RealmQuery
-import io.realm.RealmResults
 import rx.Observable
+import timber.log.Timber
 import java.util.concurrent.TimeUnit
 
-class DeezerDao {
-    companion object {
-        inline fun <reified Class: RealmObject> updateSingleItem(crossinline query: Realm.() -> RealmQuery<Class> = { where(Class::class.java) }, crossinline update: (data: Class) -> Class): Observable<Class> {
-            return createObservable {
-                updateItemTransaction(query, update)
-            }
-        }
+fun RealmConfiguration.flushDatabase(): Observable<Boolean> {
+    return startTransaction {
+        it.deleteAll()
+        Timber.d("Database has been flushed")
+        return@startTransaction true
+    }
+}
 
-        inline fun <reified Class: List<RealmObject>> saveMultipleItems(data: Class): Observable<Class> {
-            return createObservable {
-                saveMultipleItemsTransaction(data)
-            }
-        }
+inline fun <reified Class : RealmObject> RealmConfiguration.flushEntityFromDatabase(crossinline query: Realm.() -> RealmQuery<Class> = { where(Class::class.java) }): Observable<Boolean> {
+    return startTransaction {
+        it.query().findAll()?.deleteAllFromRealm()
+        return@startTransaction true
+    }
+}
 
-        inline fun <reified Class: RealmObject> saveSingleItem(data: Class): Observable<Class> {
-            return createObservable {
-                saveSingleItemTransaction(data)
-            }
-        }
-
-        inline fun <reified Class: RealmObject> getMultipleItems(crossinline query: Realm.() -> RealmQuery<Class> = { where(Class::class.java) }): Observable<List<Class>> {
-            return createObservable {
-                getMultipleItemsTransaction(query)
-            }
-        }
-
-        inline fun <reified Class: RealmObject> getSingleItem(crossinline query: Realm.() -> RealmQuery<Class> = { where(Class::class.java) }): Observable<Class> {
-            return createObservable {
-                getSingleItemTransaction(query)
-            }
-        }
-
-        // TODO check with RxJava2 if refacto is needed
-        inline fun <reified Class> createObservable(crossinline transactionResult: () -> Class?): Observable<Class> {
-            val observable : Observable<Class> = Observable.create { subscriber ->
-                subscriber.onNext(transactionResult())
-                subscriber.onCompleted()
-            }
-            return observable//.retryWhen(RetryWithDelay(3, 500))
-        }
-
-
-
-        inline fun <reified Class: RealmObject> updateItemTransaction(query: Realm.() -> RealmQuery<Class> = { where(Class::class.java) }, update: (data: Class) -> Class): Class? {
-            return startTransaction {
-                var data: Class? = it.query().findFirst()
-                if (data != null) {
-                    data = it.copyFromRealm(update(data))
-                }
-                return data
-            }
-        }
-
-        inline fun <reified Class: List<RealmObject>> saveMultipleItemsTransaction(data: Class): Class {
-            return startTransaction {
-                it.copyToRealmOrUpdate(data)
-                return data
-            }
-        }
-
-        inline fun <reified Class: RealmObject> saveSingleItemTransaction(data: Class): Class? {
-            return saveMultipleItemsTransaction(listOf(data))
-                    .flatMap {
-                        it.
-                    }
-        }
-
-        inline fun <reified Class: RealmObject> getMultipleItemsTransaction(query: Realm.() -> RealmQuery<Class> = { where(Class::class.java) }): List<Class>? {
-            return openInstance<List<Class>> {
-                val data: RealmResults<Class>? = it.query().findAll()
-
-                if (data != null) {
-                    return it.copyFromRealm(data)
-                }
-                return null
-            }
-        }
-
-        inline fun <reified Class: RealmObject> getSingleItemTransaction(query: Realm.() -> RealmQuery<Class> = { where(Class::class.java) }): Class? {
-            return openInstance<Class> {
-                val data: Class? = it.query().findFirst()
-
-                if (data != null) {
-                    return it.copyFromRealm(data)
-                }
-                return null
-            }
-        }
-
-        inline fun <ReturnType: RealmObject> startTransaction(func: (realm: Realm) -> ReturnType?): ReturnType? {
-            return openInstance {
-                //val result: ReturnType?
-                it.beginTransaction()
-                val result = func(it)
-                it.commitTransaction()
-                return@openInstance result
-            }
-        }
-
-        /**
-         * Default openInstance
-         */
-        inline fun <ReturnType> openInstance(func: (realm: Realm) -> ReturnType?): ReturnType? {
-            val realmInstance = waitForInstance()
-            val result = func(realmInstance)
-            realmInstance.close()
-            return result
-            /*var realmInstance = waitForInstance()
-            var result: ReturnType? = null
-            try {
-                //realmInstance = waitForInstance()
-                result = func(realmInstance)
-            }
-            catch (e: Throwable) {
-                Log.e("ERRRRRRRRRRROR", e.message)
-            }
-            finally {
-                realmInstance.close()
-            }
-
-            return result*/
-        }
-
-        // TODO translate sleep to Rx
-        fun waitForInstance(): Realm {
-            var instance = Realm.getDefaultInstance()
-            /*if (instance.isInTransaction) {
-                instance.refresh()
-            }*/
-            while (instance.isInTransaction) {
-                try {
-                    //delay(300)
-                    Thread.sleep(300)
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
+inline fun <reified Class : RealmObject> RealmConfiguration.fetchSingleItem(crossinline query: Realm.() -> RealmQuery<Class> = { where(Class::class.java) }): Observable<Class?> {
+    return fetchItems(query)
+            .map {
+                return@map if (it.isEmpty()) {
+                    null
+                } else {
+                    it[0]
                 }
             }
+}
 
-            if (instance.isClosed) {
-                instance = Realm.getDefaultInstance()
-            }
-
-            return instance
-        }
-
-        inline fun <reified Class: RealmObject> getSingleItemTransaction2(crossinline query: Realm.() -> RealmQuery<Class> = { where(Class::class.java) }): Observable<Class> {
-            return openInstance2 {
-                val data: Class? = it.query().findFirst()
-
-                if (data != null) {
-                    return@openInstance2 it.copyFromRealm(data)
+inline fun <reified Class : RealmObject> RealmConfiguration.saveSingleItemTransaction(data: Class): Observable<Class?> {
+    return saveMultipleItemTransaction(listOf(data))
+            .map {
+                return@map if (it.isEmpty()) {
+                    null
+                } else {
+                    it[0]
                 }
-                return@openInstance2 null
             }
+}
+inline fun <reified Class : RealmObject> RealmConfiguration.fetchItems(crossinline query: Realm.() -> RealmQuery<Class> = { where(Class::class.java) }): Observable<List<Class>> {
+    return openInstanceForFetch() {
+        val data: List<Class> = it.query().findAll()
+
+        return@openInstanceForFetch it.copyFromRealm(data)
+    }
+}
+
+inline fun <reified Class : RealmObject> RealmConfiguration.updateItemTransaction(crossinline query: Realm.() -> RealmQuery<Class> = { where(Class::class.java) }, crossinline update: (data: Class?) -> Class?): Observable<Class?> {
+    return startTransaction { realm ->
+        val data: Class? = realm.query().findFirst()
+
+        return@startTransaction data?.let {
+            val result = update(it)
+            return@let realm.copyFromRealm(result ?: throw IllegalArgumentException())
+        }
+    }
+}
+
+inline fun <reified Class : List<RealmObject>> RealmConfiguration.saveMultipleItemTransaction(data: Class): Observable<Class> {
+    return startTransaction() {
+        it.copyToRealmOrUpdate(data)
+        return@startTransaction data
+    }
+}
+
+inline fun <ReturnType> RealmConfiguration.startTransaction(crossinline func: (realm: Realm) -> ReturnType): Observable<ReturnType> {
+    return openInstanceForTransaction() { it ->
+        var result: ReturnType? = null
+        it.executeTransaction {
+            result = func(it)
         }
 
-        inline fun <reified Class: RealmObject> updateItemTransaction2(crossinline query: Realm.() -> RealmQuery<Class> = { where(Class::class.java) }, crossinline update: (data: Class?) -> Class?): Observable<Class?> {
-            return startTransaction2 {
-                var data: Class? = it.query().findFirst()
-                if (data != null) {
-                    data = update(data)
-                    data = it.copyFromRealm(data ?: throw IllegalArgumentException())
-                }
-                return@startTransaction2 data
-            }
-        }
+        return@openInstanceForTransaction result ?: throw NullPointerException()
+    }
+}
 
-        inline fun <reified Class: RealmObject> saveSingleItemTransaction2(data: Class): Observable<Class?> {
-            return startTransaction2 {
-                it.copyToRealmOrUpdate(data)
-                return@startTransaction2 data
-            }
-        }
+inline fun <reified ReturnType> RealmConfiguration.openInstanceForFetch(crossinline func: (realm: Realm) -> ReturnType?): Observable<ReturnType> {
+    return waitForInstance().flatMap {
+        val results = func(it)
+        it.close()
 
-        inline fun <ReturnType: RealmObject> startTransaction2(crossinline func: (realm: Realm) -> ReturnType?): Observable<ReturnType?> {
-            return openInstance2 {
-                var result : ReturnType? = null
-                it.executeTransaction {
-                    result = func(it)
-                }
-                return@openInstance2 result
-            }
-        }
+        return@flatMap results?.let { result ->
+            Observable.just(result)
+        } ?: Observable.error(Throwable(ReturnType::class.toString()))
+    }
+}
 
-        inline fun <ReturnType> openInstance2(crossinline func: (realm: Realm) -> ReturnType?): Observable<ReturnType> {
-            return waitForInstance2().flatMap {
-                val results = func(it)
-                it.close()
-                return@flatMap Observable.just(results)
-            }
-        }
+inline fun <ReturnType> RealmConfiguration.openInstanceForTransaction(crossinline func: (realm: Realm) -> ReturnType): Observable<ReturnType> {
+    return waitForInstance().flatMap {
+        val results = func(it)
+        it.close()
+        return@flatMap Observable.just(results)
+    }
+}
 
-        fun waitForInstance2(): Observable<Realm> {
-            var retries = 0
-            val observable = createObservable {
-                return@createObservable Realm.getDefaultInstance()
-            }
-            return observable.flatMap {
-                if (it.isInTransaction
-                    && retries < 3) {
-                    return@flatMap Observable.timer(300, TimeUnit.MILLISECONDS)
-                            .flatMap {
-                                retries += 1
-                                return@flatMap waitForInstance2()
-                            }
-                }
-                else if (it.isClosed) {
-                    return@flatMap Observable.just(Realm.getDefaultInstance())
-                }
-                return@flatMap Observable.just(it)
-            }
+fun RealmConfiguration.waitForInstance(retries: Int = 0): Observable<Realm> {
+    val observable = createObservable {
+        return@createObservable Realm.getInstance(this)
+    }
+    return observable.flatMap {
+        if (it.isInTransaction
+                && retries < 3) {
+            return@flatMap Observable.timer(300, TimeUnit.MILLISECONDS)
+                    .flatMap { waitForInstance(retries + 1) }
+        } else if (it.isClosed) {
+            return@flatMap Observable.just(Realm.getInstance(this))
         }
+        return@flatMap Observable.just(it)
+    }
+}
+
+inline fun <reified Class> createObservable(crossinline transactionResult: () -> Class?): Observable<Class> {
+    return Observable.create { subscriber ->
+        subscriber.onNext(transactionResult())
+        subscriber.onCompleted()
     }
 }
